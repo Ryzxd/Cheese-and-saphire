31a3b9ad028f3687d1715c0e5840c71e3601e6cf
TimbaCdnis2468 <macariuschan@10.130.21.2>
2022-01-20 09:38:30 +0800

    i watch bob the builder

--- src/main/java/net/mcreator/tntgoboom/world/features/ores/PotehtohOreFeature.java
+++ src/main/java/net/mcreator/tntgoboom/world/features/ores/PotehtohOreFeature.java
@@ -1,13 +1,33 @@
 
 package net.mcreator.tntgoboom.world.features.ores;
 
-public class PotehtohOreFeature extends OreFeature {
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTestType;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.heightproviders.UniformHeight;
+import net.minecraft.world.level.levelgen.feature.configurations.RangeDecoratorConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.OreFeature;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.Registry;
 
+import net.mcreator.tntgoboom.init.TntGoBoomModBlocks;
+
+import java.util.Set;
+import java.util.Random;
+
+public class PotehtohOreFeature extends OreFeature {
 	public static final PotehtohOreFeature FEATURE = (PotehtohOreFeature) new PotehtohOreFeature().setRegistryName("tnt_go_boom:potehtoh_ore");
 	public static final ConfiguredFeature<?, ?> CONFIGURED_FEATURE = FEATURE
 			.configured(new OreConfiguration(PotehtohOreFeatureRuleTest.INSTANCE, TntGoBoomModBlocks.POTEHTOH_ORE.defaultBlockState(), 7))
 			.range(new RangeDecoratorConfiguration(UniformHeight.of(VerticalAnchor.absolute(1), VerticalAnchor.absolute(63)))).squared().count(11);
-
 	public static final Set<ResourceLocation> GENERATE_BIOMES = null;
 
 	public PotehtohOreFeature() {
@@ -18,37 +38,28 @@ public class PotehtohOreFeature extends OreFeature {
 		WorldGenLevel world = context.level();
 		ResourceKey<Level> dimensionType = world.getLevel().dimension();
 		boolean dimensionCriteria = false;
-
 		if (dimensionType == Level.OVERWORLD)
 			dimensionCriteria = true;
-
 		if (!dimensionCriteria)
 			return false;
-
 		return super.place(context);
 	}
 
 	private static class PotehtohOreFeatureRuleTest extends RuleTest {
-
 		static final PotehtohOreFeatureRuleTest INSTANCE = new PotehtohOreFeatureRuleTest();
 		static final com.mojang.serialization.Codec<PotehtohOreFeatureRuleTest> codec = com.mojang.serialization.Codec.unit(() -> INSTANCE);
-
 		static final RuleTestType<PotehtohOreFeatureRuleTest> CUSTOM_MATCH = Registry.register(Registry.RULE_TEST,
 				new ResourceLocation("tnt_go_boom:potehtoh_ore_match"), () -> codec);
 
 		public boolean test(BlockState blockAt, Random random) {
 			boolean blockCriteria = false;
-
 			if (blockAt.getBlock() == Blocks.STONE)
 				blockCriteria = true;
-
 			return blockCriteria;
 		}
 
 		protected RuleTestType<?> getType() {
 			return CUSTOM_MATCH;
 		}
-
 	}
-
 }
